4d042d81b0572cf6cc4e6f06b886a03735dc5917
carvalhoclarieli <145785057+carvalhoclarieli@users.noreply.github.com>
2025-11-25 12:12:48 -0300

    25/11

--- aleatorio.js
+++ aleatorio.js
@@ -1,4 +1,8 @@
+const nomes = ["Clarieli", "Claudia", "Odete", "Leticia", "Chrislane", "Djenifer", "Gabriely", "Pamela"];
+
 export function aleatorio(lista){
     const posicao = Math.floor(Math.random()*lista.length);
     return lista[posicao];
-}                                                                                                                                                                             
+} 
+
+export const nome = aleatorio(nomes);
